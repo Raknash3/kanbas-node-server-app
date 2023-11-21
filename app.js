17dc3154d1e94d8fa52c7249fdd75ccb9d264ae2
Ravi Shankar <ivarraknash3@gmail.com>
2023-11-21 06:09:38 -0500

    Made changes to app.js

--- app.js
+++ app.js
@@ -16,4 +16,8 @@ CourseRoutes(app);
 AssignmentRoutes(app)
 Hello(app)
 Lab5(app);
-app.listen(process.env.PORT || 4000);
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
